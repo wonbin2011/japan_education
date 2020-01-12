55a437beceb3d6b5e62b0077035abd54fc02090c
root <root@localhost.localdomain>
2020-01-11 21:43:03 -0500

    config cleardb in better way

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'heroku_cleardb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -43,17 +43,28 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $url['host'],
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => substr($url["path"], 1),
-            'username' => $url['user'],
-            'password' => $url['pass'],
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+        ],
+
+        'heroku_cleardb' => [
+            'driver' => 'mysql',
+            'host' => $url['host'],
+            'database' => substr($url["path"], 1),
+            'username' => $url['user'],
+            'password' => $url['pass'],
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
         ],
 
         'pgsql' => [
